8a1cada0d12c47e83aaf52bd2efbe8839ade7026
kluge121 <kluge0221@gmail.com>
2018-10-02 21:50:42 +0900

    Main - Feed, RecyclerView

--- app/src/main/java/com/globe/havebeen/view/main/MainFeedFragment.kt
+++ app/src/main/java/com/globe/havebeen/view/main/MainFeedFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/globe/havebeen/view/main/adapter/FeedStoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/globe/havebeen/view/main/adapter/FeedStoryRecyclerViewAdapter.kt
@@ -2,33 +2,67 @@ package com.globe.havebeen.view.main.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.globe.havebeen.R
+import kotlinx.android.synthetic.main.recyclerview_item_feed_story.view.*
 
 
-class FeedStoryRecyclerViewAdapter(context : Context) : RecyclerView.Adapter<FeedStoryViewHolder>() {
+class FeedStoryRecyclerViewAdapter(var context: Context) : RecyclerView.Adapter<FeedStoryViewHolder>() {
 
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
+        return (position % 2)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedStoryViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val view = LayoutInflater.from(context).inflate(R.layout.recyclerview_item_feed_story, parent, false)
+
+        if (viewType == 1) {
+
+            return DoingStoryViewHolder(view)
+        } else {
+            return LastStoryViewHolder(view)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 5
     }
 
     override fun onBindViewHolder(holder: FeedStoryViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.setView()
+
     }
 
 
 }
 
 
-abstract class FeedStoryViewHolder(view : View) : RecyclerView.ViewHolder(view){
+abstract class FeedStoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     abstract fun setView()
 }
+
+
+class DoingStoryViewHolder(view: View) : FeedStoryViewHolder(view) {
+
+    var imageView: ImageView = view.feedStoryIv
+    override fun setView() {
+        imageView.setImageResource(R.drawable.logo)
+
+    }
+}
+
+class LastStoryViewHolder(view: View) : FeedStoryViewHolder(view) {
+
+    var imageView: ImageView = view.feedStoryIv
+    override fun setView() {
+        imageView.setImageResource(R.drawable.a2)
+
+    }
+
+}
+
 
